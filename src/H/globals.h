417ee28eac84513461a4e380bbc271ee4e6179e4
Baron-von-Riedesel <japhethx@gmail.com>
2020-03-14 15:08:55 +0100

    jwasm release v2.13

--- src/H/globals.h
+++ src/H/globals.h
@@ -186,7 +186,7 @@ char *strupr(char *);
 /* JWasm version info */
 #define _JWASM_VERSION_STR_ "2.13"
 #define _JWASM_VERSION_INT_ 213
-#define _JWASM_VERSION_SUFFIX_ "pre"
+#define _JWASM_VERSION_SUFFIX_ ""
 #define _JWASM_VERSION_ _JWASM_VERSION_STR_ _JWASM_VERSION_SUFFIX_
 
 #define NULLC  '\0'
